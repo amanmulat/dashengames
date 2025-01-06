8f0402c3ce7b7550b03895f2fcea4f30a9bce04d
amanmulat <amanuaelabegaz@gmail.com>
2025-01-06 07:12:46 +0300

    more changes

--- src/pages/game/Welcome.tsx
+++ src/pages/game/Welcome.tsx
@@ -14,37 +14,37 @@ const Welcome = () => {
         backgroundPosition: 'top',
       }}
     >
-      <img
-        src={Beer}
-        alt="profile cover"
-        className="w-[400px]  rounded-tl-sm rounded-tr-sm object-cover object-center absolute bottom-0 right-0"
-      />
       <div className="text-center">
         <h1 className="text-3xl font-bold mb-4 text-white">
           Welcome <br />
           Choose a Challenge
         </h1>
-        <div className="flex justify-center items-center gap-3">
+        <div className="flex justify-center items-center gap-3 z-1">
           <Link
             to={'/puzzle'}
-            className="bg-blue-500 text-white px-6 py-2 rounded shadow hover:bg-blue-600 transition"
+            className="bg-blue-500 text-white px-6 py-2 rounded shadow hover:bg-blue-600 transition z-10"
           >
             Puzzle
           </Link>
           <Link
             to={'/trivia'}
-            className="bg-blue-500 text-white px-6 py-2 rounded shadow hover:bg-blue-600 transition"
+            className="bg-blue-500 text-white px-6 py-2 rounded shadow hover:bg-blue-600 transition z-10"
           >
             Trivia
           </Link>
           <Link
             to={'/spinner'}
-            className="bg-blue-500 text-white px-6 py-2 rounded shadow hover:bg-blue-600 transition"
+            className="bg-blue-500 text-white px-6 py-2 rounded shadow hover:bg-blue-600 transition z-10"
           >
             Spinner Wheel
           </Link>
         </div>
       </div>
+      <img
+        src={Beer}
+        alt="profile cover"
+        className="w-[200px]  rounded-tl-sm rounded-tr-sm object-cover object-center absolute bottom-0 right-0  z-0 sm:w-[300px] lg:w-[400px]"
+      />
     </div>
   );
 };
